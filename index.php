2d68f1ad09bfca99336dd0eb04cdeb7bd8f7409a
Mcweii <153883532+Mcweii@users.noreply.github.com>
2024-11-23 01:34:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+   <html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!--=============== REMIXICONS ===============-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
+
+      <!--=============== CSS ===============-->
+      <link rel="stylesheet" href="assets/css/styles.css">
+
+      <title>Car Responsive Card</title>
+   </head>
+   <body>
+      <div class="container">
+         <div class="card__container">
+            <article class="card__article">
+               <img src="assets/img/car-1.png" alt="image" class="card__img">
+
+               <div class="card__data">
+                  <h3 class="card__title">Black McLaren <br> 570S GT4</h3>
+                  <span class="card__price">$200,000</span>
+               </div>
+
+               <img src="assets/img/bg-card.jpg" alt="image" class="card__bg">
+               
+            </article>
+
+            <article class="card__article">
+               <img src="assets/img/car-2.png" alt="image" class="card__img">
+
+               <div class="card__data">
+                  <h3 class="card__title">Red McLaren <br> 650S Can Am Race Car</h3>
+                  <span class="card__price">$392,308</span>
+               </div>
+
+               <img src="assets/img/bg-card.jpg" alt="image" class="card__bg">
+
+            </article>
+
+            <article class="card__article">
+               <img src="assets/img/car-3.png" alt="image" class="card__img">
+
+               <div class="card__data">
+                  <h3 class="card__title">McLaren F1 <br> LM </h3>
+                  <span class="card__price">$20.465 million</span>
+               </div>
+
+               <img src="assets/img/bg-card.jpg" alt="image" class="card__bg">
+
+            </article>
+         </div>
+      </div>
+   </body>
+</html>
